5e01fc3c6871cd29bbf01d78b7b98f0a6a5008b6
matudev9 <matudev99@gmail.com>
2023-11-29 16:01:20 -0300

    update dbz pages and app.modules.ts

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MainPageComponent } from './pages/main-page.component';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainPageComponent,
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    MainPageComponent
   ]
 })
 
